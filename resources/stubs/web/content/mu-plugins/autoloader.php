f5b4cf043bf5cb49e91463b30a1f370366a5f9f9
Nasyrov Evgenii <inasyrov@ya.ru>
2019-06-08 15:36:29 +0100

    Add mu-plugins autoloader implementation

--- resources/stubs/web/content/mu-plugins/autoloader.php
+++ resources/stubs/web/content/mu-plugins/autoloader.php
@@ -1,1 +1,39 @@
 <?php
+/*
+Plugin Name:    Helick Autoloader
+Description:    The Helick autoloader enables standard plugins to be loaded as must-use ones.
+Author:         Evgenii Nasyrov
+Author URI:     https://helick.io/
+*/
+
+use const Helick\CMS\ROOT_DIR;
+
+/**
+ * Load the required must-use plugins.
+ */
+$requiredPlugins = require ROOT_DIR . '/bootstrap/cache/required-mu-plugins.php';
+
+array_walk($requiredPlugins, static function (string $plugin) {
+    require __DIR__ . DIRECTORY_SEPARATOR . $plugin;
+});
+
+unset($requiredPlugins);
+
+/**
+ * Adjust admin view to display must-use plugins.
+ */
+add_filter('show_advanced_plugins', function (bool $show, string $type) {
+    $screen = get_current_screen();
+
+    $isRequiredScreen = in_array($screen->base, ['plugins-network', 'plugins'], true);
+    $isRequiredType   = $type === 'mustuse';
+    $isUserAllowed    = current_user_can('activate_plugins');
+
+    if (!$isRequiredScreen || !$isRequiredType || !$isUserAllowed) {
+        return $show;
+    }
+
+    $GLOBALS['plugins']['mustuse'] = require ROOT_DIR . '/bootstrap/cache/mu-plugins.php';
+
+    return false;
+}, 0, 2);
